130a41f6936d60941c59fa6471d8479b786d3cf0
Trev <dr4g0n@gmail.com>
2020-08-26 15:36:29 -0400

    healthbar and buy button basics done

--- src/components/shared/UnitList.jsx
+++ src/components/shared/UnitList.jsx
@@ -1,43 +1,11 @@
 import React from 'react';
 import UnitCard from './UnitCard';
-import BuyUnitButton from './BuyUnitButton';
-import HealthBar from './HealthBar';
-import SelectUnitButton from './SelectUnitButton';
 
-export const unitSubComponent = {
-    BUYBUTTON: 'buybutton',
-    HEALTHBAR: 'healthbar',
-    SELECTBUTTON: 'selectbutton',
-    ENEMY: 'enemy'
-}
-
-export default function UnitList({units, subComponentType, handleBuyUnit, unitHealth, handleSelectUnit}) {
-
-    const subComponent = (type) =>
-    {
-        switch (type) {
-            case 'buybutton':
-                return <BuyUnitButton onClick={handleBuyUnit}/>  
-
-            case 'healthbar':
-                return <HealthBar unitHealth={unitHealth}/>
-        
-            case 'selectbutton':
-                return <SelectUnitButton handleSelectUnit={handleSelectUnit}/>
-
-            case 'enemy':
-                return <HealthBar unitHealth={unitHealth}/>
-
-            default:
-                console.log("Soldier List style enum error")
-                break;
-        }
-    }
-
+export default function UnitList({units, unitSubComponentType, handleBuyUnit, unitHealth, handleSelectUnit}) {
   return (
     <div className="flex flex-col w-1/2 bg-gray-100">
         {units.map((unit,id)=>(
-            <UnitCard key={id} data={unit} subComponent={subComponent(subComponentType)} enemy={subComponentType=='enemy'}/>
+            <UnitCard key={id} data={unit} unitSubComponentType={unitSubComponentType} enemy={unitSubComponentType==='enemy'}/>
         ))}
     </div>
   );
